2153c57acab2c72bb1dcb68f7f20a346c7376030
CleytonDevBr <CleytonDevs@gmail.com>
2022-06-29 16:38:19 -0300

    12

---
+++ javascript.js
@@ -0,0 +1,15 @@
+ const amburguer = document.querySelector(".amburguer");
+ const menu = document.querySelector(".menu");
+
+
+ amburguer.addEventListener("click", () => {
+ amburguer.classList.toggle("active");
+ menu.classList.toggle("active");
+ })
+
+
+
+ 
+
+
+ 
